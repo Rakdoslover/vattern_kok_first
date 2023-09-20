b05d87a5e04998756696ff0d5b533772e3dc8731
Oscar Lundsör <oscarlundsor@gmail.com>
2023-09-20 07:56:33 +0000

    added a HTML base for the site to lean upon

--- vattern_kok/settings.py
+++ vattern_kok/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8000-rakdoslover-vatternkokf-tvkupoqs1ez.ws-eu104.gitpod.io/'
+    '8000-rakdoslover-vatternkokf-4r5u2tw7qxu.ws-eu104.gitpod.io'
 ]
 
 
