9cc029736d085a23cfb49339b283c89a8ef3a925
Oscar Lundsör <oscarlundsor@gmail.com>
2023-11-20 14:16:56 +0000

    changes to the DIRS folder in settings

--- vattern_kok/settings.py
+++ vattern_kok/settings.py
@@ -79,8 +79,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            TEMPLATES_DIR,
-            BASE_DIR / 'templates',
+            os.path.join(BASE_DIR, 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
