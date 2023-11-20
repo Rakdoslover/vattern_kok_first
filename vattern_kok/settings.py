3d074eb648b2e57be662d064dde7f7806ec485c8
Oscar Lundsör <oscarlundsor@gmail.com>
2023-11-20 07:54:24 +0000

    added a python library to store the envs, trying to push this one to the hub

--- vattern_kok/settings.py
+++ vattern_kok/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
-from pathlib import Path
-if os.path.exists("env.py"):
-    import env
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEBUG' in os.environ
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = [
     '8000-rakdoslover-vatternkokf-qzkncq3yhyr.ws-eu105.gitpod.io',
