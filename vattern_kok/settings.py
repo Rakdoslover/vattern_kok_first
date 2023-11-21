9cb372eb7c7d649d341b1d6c383860c74b11f2f4
Oscar Lundsör <oscarlundsor@gmail.com>
2023-11-21 08:56:09 +0000

    change settings

--- vattern_kok/settings.py
+++ vattern_kok/settings.py
@@ -15,7 +15,6 @@ import os
 from dotenv import load_dotenv
 import mimetypes
 mimetypes.add_type("text/css", ".css", True)
-mimetypes.add_type("text/html", ".html", True)
 if os.path.isfile('env.py'):
     import env
 
