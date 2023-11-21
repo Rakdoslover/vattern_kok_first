66d37727de6f984590f93dcddb019d680d6931e4
Oscar Lundsör <oscarlundsor@gmail.com>
2023-11-21 09:05:04 +0000

    added djanog_heroku

--- vattern_kok/settings.py
+++ vattern_kok/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 from dotenv import load_dotenv
 import mimetypes
 mimetypes.add_type("text/css", ".css", True)
